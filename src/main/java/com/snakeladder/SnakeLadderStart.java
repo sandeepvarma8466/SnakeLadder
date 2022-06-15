e256e6e7ad45c84c663eee1a0987203b372b146f
sandeepvarma8466 <svarma852@gmail.com>
2022-06-15 13:26:49 +0530

    single player start

---
+++ src/main/java/com/snakeladder/SnakeLadderStart.java
@@ -0,0 +1,9 @@
+package com.snakeladder;
+
+public class SnakeLadderStart {
+    public static void main(String[] args) {
+        int position = 0;
+        System.out.println("Single player in");
+        System.out.println("Game start with position : " +position);
+    }
+}
